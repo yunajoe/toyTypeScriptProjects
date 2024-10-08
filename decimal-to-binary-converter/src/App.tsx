e13cb45fbebe59846998f59173d734d135ef335f
yunajoe <yunajoe@gmail.com>
2024-10-08 16:21:19 +0900

    fix: callstakk고치는중

--- decimal-to-binary-converter/src/App.tsx
+++ decimal-to-binary-converter/src/App.tsx
@@ -9,6 +9,7 @@ type Item = {
 function App() {
   const [inputValue, setInputValue] = useState<number>();
   const [binaryValue, setBinaryValue] = useState("");
+
   const [callValueArr, setCallValueArr] = useState<number[]>([]);
   const [renderValueArr, setRenderValueArr] = useState<Item[]>([]);
 
@@ -29,6 +30,7 @@ function App() {
     const binaryValue = convertDecimalToBinary(value);
     setBinaryValue(binaryValue);
   };
+
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       if (!inputValue) {
@@ -45,8 +47,9 @@ function App() {
   };
 
   const callStackAnimation = (arr: number[]) => {
-    setBinaryValue("Call Stack Animation");
-    let defaultTime = 2000;
+    // setBinaryValue("Call Stack Animation");
+    console.log("CallStackAnimation입니당", arr);
+    let defaultTime = 1000;
 
     arr.forEach((value: number, index: number) => {
       let delayTime = defaultTime * (index + 1);
@@ -68,50 +71,105 @@ function App() {
     }
   }, [callValueArr]);
 
-  useEffect(() => {
-    if (renderValueArr.length > 0 && callValueArr.length > 0)
-      if (renderValueArr.length === callValueArr.length) {
-        let maxTime = renderValueArr[0].time;
-        renderValueArr.forEach((item, index) => {
-          let delayTime = maxTime + 1000 * (index + 1);
-          setTimeout(() => {
-            setRenderValueArr((prev) => {
-              return prev.map((prevItem, prevIndex) => {
-                if (prevIndex === index) {
-                  return {
-                    ...prevItem,
-                    callValue: `decimalTobinary(${prevItem.callValue}) returns and gives that value to the stack below. then if pops off the stack`,
-                  };
-                }
-                return prevItem;
-              });
+  const changeStack = (arr: Item[]) => {
+    if (arr.length === 0) {
+      return;
+    }
+
+    console.log("arr", arr);
+
+    arr.forEach((_, index) => {
+      let maxTime = arr[0].time;
+      let delayTime = maxTime + 1000 * (index + 1);
+
+      if (index === 0) {
+        setTimeout(() => {
+          setRenderValueArr((prev) => {
+            return prev.map((prevItem, prevIndex) => {
+              if (prevIndex === 0) {
+                return {
+                  ...prevItem,
+                  callValue: `decimalTobinary(${prevItem.callValue}) returns and gives that value to the stack below. then if pops off the stack`,
+                };
+              }
+              return prevItem;
             });
-          }, delayTime);
-        });
+          });
+        }, delayTime);
+
+        setTimeout(() => {
+          console.log("하이이이이");
+          setRenderValueArr((prev) => {
+            return prev.filter((_, prevIndex) => prevIndex !== 0);
+          });
+          setCallValueArr((prev) => {
+            return prev.filter((_, prevIndex) => prevIndex !== 0);
+          });
+        }, delayTime + 3000);
       }
-  }, [renderValueArr.length]);
-
-  console.log("렌더되는 arr입니다", renderValueArr);
+    });
+  };
 
   useEffect(() => {
-    if (renderValueArr.length > 0) {
-      const isAllString = renderValueArr.every(
-        (item) => typeof item.callValue === "string"
-      );
-      let maxTime = renderValueArr[0].time;
-      if (isAllString) {
-        renderValueArr.forEach((_, index) => {
-          let delayTime = maxTime + 1000 * (index + 1);
-          setTimeout(() => {
-            setRenderValueArr((prev) => {
-              const filterArr = prev.filter((_, prevIndex) => prevIndex !== 0);
-              return filterArr;
-            });
-          }, delayTime + 1000 * (index + 1));
-        });
+    console.log(
+      "렌더엉어",
+      renderValueArr,
+      renderValueArr.length,
+      callValueArr.length
+    );
+    if (renderValueArr.length > 0 && callValueArr.length > 0) {
+      if (renderValueArr.length === callValueArr.length) {
+        console.log("renderValu", renderValueArr);
+        changeStack(renderValueArr);
       }
     }
-  }, [renderValueArr]);
+  }, [renderValueArr.length]);
+
+  // useEffect(() => {
+  //   if (renderValueArr.length > 0 && callValueArr.length > 0) {
+  //     if (renderValueArr.length === callValueArr.length) {
+  //       console.log("rrr", renderValueArr);
+  //       let maxTime = renderValueArr[0].time;
+  //       renderValueArr.forEach((item, index) => {
+  //         let delayTime = maxTime + 1000 * (index + 1);
+  //         let timeId = setTimeout(() => {
+  //           setRenderValueArr((prev) => {
+  //             return prev.map((prevItem, prevIndex) => {
+  //               if (prevIndex === index) {
+  //                 return {
+  //                   ...prevItem,
+  //                   callValue: `decimalTobinary(${prevItem.callValue}) returns and gives that value to the stack below. then if pops off the stack`,
+  //                 };
+  //               }
+  //               return prevItem;
+  //             });
+  //           });
+  //         }, delayTime);
+  //         console.log("timeId", timeId);
+  //       });
+  //     }
+  //   }
+  // }, [renderValueArr.length]);
+
+  // useEffect(() => {
+  //   if (renderValueArr.length > 0) {
+  //     const isAllString = renderValueArr.every(
+  //       (item) => typeof item.callValue === "string"
+  //     );
+  //     let maxTime = renderValueArr[0].time;
+  //     if (isAllString) {
+  //       renderValueArr.forEach((_, index) => {
+  //         let delayTime = maxTime + 1000 * (index + 1);
+  //         setTimeout(() => {
+  //           setRenderValueArr((prev) => {
+  //             const filterArr = prev.filter((_, prevIndex) => prevIndex !== 0);
+  //             return filterArr;
+  //           });
+  //         }, delayTime + 1000 * (index + 1));
+  //       });
+  //     }
+  //   }
+  // }, [renderValueArr]);
 
   return (
     <>
