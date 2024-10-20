09609754d24c8deaffc458f1bbd3a01709969f87
yunajoe <yunajoe@gmail.com>
2024-10-20 16:03:04 +0900

    feat: player만드는중

--- platformer-game/src/components/Player.tsx
+++ platformer-game/src/components/Player.tsx
@@ -1,4 +1,17 @@
 function Player() {
+  // player에게 필요한건?
+  // player의 크기 (width, height)
+  // playerr의 현재 위치
+  // player의 움직인 거리
+  class Player {
+    width: number;
+    height: number;
+
+    constructor(width: number, height: number) {
+      this.width = width;
+      this.height = height;
+    }
+  }
   return <div></div>;
 }
 
